ce1ce9905985f312b48f9034e3434aaa0a5b960a
m-yahya <yahya_gis@hotmail.com>
2019-11-19 11:38:42 +0100

    Add oracle contract skeleton

---
+++ contracts/OracleContract.sol
@@ -0,0 +1,12 @@
+pragma solidity ^>=0.4.16 <0.6.0;
+
+contract OracleContract {
+
+    // register oracle function
+
+    // request data function
+
+    // submit oracle response function
+
+    // utility functions
+}
