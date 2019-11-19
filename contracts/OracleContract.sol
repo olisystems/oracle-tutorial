a088ea7fa7216d25fb7f68356fc47b988ac91d48
m-yahya <yahya_gis@hotmail.com>
2019-11-19 12:32:28 +0100

    Add utility functions

--- contracts/OracleContract.sol
+++ contracts/OracleContract.sol
@@ -9,4 +9,35 @@ contract OracleContract {
     // submit oracle response function
 
     // utility functions
+
+    // generate indexes
+    function generateIndexes(address account) internal returns (uint8[3]){
+        uint8[3] memory indexes;
+        // first random index
+        indexes[0] = getRandomIndex(account);
+        // 2nd random index
+        indexes[1] = indexes[0];
+        // check if 2nd index is same, then get another random
+        while (indexes[1] == indexes[0]) {
+            indexes[1] = getRandomIndex(account);
+        }
+        // 3rd random index
+        indexes[2] = indexes[1];
+        while ((indexes[2] == indexes[0]) || (indexes[2] == indexes[1])) {
+            indexes[2] = getRandomIndex(account);
+        }
+        return indexes;
+    }
+
+    // generate a random number from 0-9
+    function getRandomIndex(address accounts) internal returns (uint8){
+        uint8 maxValue = 10;
+        uint8 random = uint8(uint256(keccak256(abi.encodePacked(blockhash(block.number - nonce++), account))) % maxValue);
+
+        // solidity can fetch hashes for last 256 blocks
+        if (nonce > 250) {
+            nonce = 0;
+        }
+        return random;
+    }
 }
