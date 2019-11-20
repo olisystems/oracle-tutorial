005949391751179bc99412a5203729476bc900d8
m-yahya <yahya_gis@hotmail.com>
2019-11-20 12:03:07 +0100

    Add fetchTokenPrice function

--- contracts/OracleContract.sol
+++ contracts/OracleContract.sol
@@ -14,6 +14,19 @@ contract OracleContract {
     // keep the record of all oracles
     mapping(address => uint8[3]) oracles;
 
+    // model for responses from oracles
+    struct ResponseInfo {
+        address requester;
+        bool isOpen;
+        mapping(uint8 => address[]) responses;
+    }
+
+    // keep record of all oracle responses
+    mapping(bytes32 => ResponseInfo)private oracleResponses;
+
+    // event fired when a data request is made
+    event DataRequest(uint8 index, string token, uint256 timestamp);
+
     constructor()public{
         owner = msg.sender;
     }
@@ -28,7 +41,7 @@ contract OracleContract {
     function registerOracle() external payable {
         // require registration fee
         require(msg.value >= REGISTRATION_FEE, 'Registration fee is required');
-        
+
         // get random indexes
         uint8[3] memory indexes = generateIndexes(msg.sender);
         // assign the indexes to oracles
@@ -36,11 +49,25 @@ contract OracleContract {
     }
 
     // get oracles
-    function getOracle(address account)external view requireContractOwner returns(uint8[3] memory){
+    function getOracle(address account) external view requireContractOwner returns (uint8[3] memory){
         return oracles[account];
     }
 
     // request data function
+    function fetchTokenPrice(string token, uint256 timestamp) external {
+        // generate a random number between 0-9 which oracle may respond
+        uint8 index = getRandomIndex(msg.sender);
+
+        // generate a unique key and assign it to the request
+        bytes32 key = keccak256(abi.encodePacked(index, token, timestamp));
+        oracleResponses[key] = ResponseInfo({
+            requester : msg.sender,
+            isOpen : true
+            });
+
+        // emit the event to notify the oracles that match the index
+        emit DataRequest(index, token, timestamp);
+    }
 
     // submit oracle response function
 
